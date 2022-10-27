76729e623b4900fe55c660b44091e815842fa663
hdi0b <977868425@qq.com>
2022-10-27 16:35:48 +0800

    add exit menuitem

--- EXETray/EXETrayForm.cs
+++ EXETray/EXETrayForm.cs
@@ -5,12 +5,18 @@ namespace EXETray
         public EXETrayForm()
         {
             InitializeComponent();
-           
+            InitializeEexTray();
         }
 
         private void EXETrayForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void InitializeEexTray()
+        {
+            this.EXETray.ContextMenuStrip = new ContextMenuStrip();
+            this.EXETray.ContextMenuStrip.Items.Add("Exit", null, (sender, args) => this.Close());
         }
 
         private void EXETray_MouseClick(object sender, MouseEventArgs e)
